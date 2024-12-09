83103cd8dc2dfdb0ca79d654c22e5b04d59a931f
Lenadoss <odanelfrias@gmail.com>
2024-12-09 19:05:31 -0400

    Segundo commit.

---
+++ EjemplosConsolaClase/Perro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosConsolaClase
+{
+    internal class Perro
+    {
+        private string Nombre;
+        private string Color;
+        private string Origen;
+        public Perro(string Nombre, string Color, string Origen)
+        {
+            this.Nombre = Nombre;
+            this.Color = Color;
+            this.Origen = Origen;
+        }
+
+        public string nombre
+        {
+            get { return Nombre; }
+        }
+
+        public string color 
+        {
+            get { return Color; }
+        }
+
+        public string origen
+        {
+            get { return Origen; }
+        }
+        public void Ladrar()
+        {
+            Console.WriteLine("Estoy ladrando...");
+        }
+    }
+}
---
+++ EjemplosConsolaClase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosConsolaClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nom;
+            string Col;
+            string Ori;
+
+            Console.Write("Ingrese el nombre del perro:");
+            Nom = Console.ReadLine();
+
+            Console.Write("Ingrese el color del perro:");
+            Col = Console.ReadLine();
+
+            Console.Write("Ingrese el origen del perro:");
+            Ori = Console.ReadLine();
+
+            var perro1 = new Perro(Nom, Col, Ori);
+            
+            
+            Console.WriteLine("Esta es la información del perro: "+ perro1.nombre +", " + perro1.color + ", " + perro1.origen);
+
+            perro1.Ladrar();
+        }
+    }
+}
