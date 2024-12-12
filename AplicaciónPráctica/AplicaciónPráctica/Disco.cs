b8a70fff730cdfffbc478dc4cfa25828ea08ded3
Lenadoss <odanelfrias@gmail.com>
2024-12-12 10:00:12 -0400

    Ejercicio asignado con base de datos DISCOS

---
+++ AplicaciónPráctica/AplicaciónPráctica/Disco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaciónPráctica
+{
+    internal class Disco
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public int CantidadCanciones { get; set; }
+        public string UrlImagen { get; set; }
+        public string Estilo { get; set; }
+        public string Edicion { get; set; }
+
+        public Disco(int Id, string Titulo, DateTime FechaLanzamiento, int CantidadCanciones, string UrlImagen, string Estilo, string Edicion)
+        {
+            this.Id = Id;
+            this.Titulo = Titulo;
+            this.FechaLanzamiento = FechaLanzamiento;
+            this.CantidadCanciones = CantidadCanciones;
+            this.UrlImagen = UrlImagen;
+            this.Estilo = Estilo;
+            this.Edicion = Edicion;
+        }
+    }
+}
---
+++ AplicaciónPráctica/AplicaciónPráctica/DiscoNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaciónPráctica
+{
+    internal class DiscoNegocio
+    {
+        public List<Disco> Listar()
+        {
+            List<Disco> Lista = new List<Disco>();
+            SqlConnection Conexion = new SqlConnection();
+            SqlCommand Comando = new SqlCommand();
+            SqlDataReader Lector;
+
+            try
+            {
+                Conexion.ConnectionString = "Server = ODANEL\\SQLEXPRESS; DataBase = DISCOS_DB; Integrated Security = true";
+                Comando.CommandType = System.Data.CommandType.Text;
+                Comando.CommandText = "SELECT D.Id, D.Titulo, D.FechaLanzamiento, D.CantidadCanciones, D.UrlImagenTapa, E.Descripcion, T.Descripcion AS EDICION FROM DISCOS AS D, ESTILOS AS E, TIPOSEDICION AS T WHERE D.IdEstilo = E.Id AND D.IdTipoEdicion = T.Id";
+                Comando.Connection = Conexion;
+                Conexion.Open();
+                Lector = Comando.ExecuteReader();
+
+                while (Lector.Read())
+                {
+                    int Id = (int)Lector["Id"];
+                    string Titulo = (string)Lector["Titulo"];
+                    DateTime FechaLanzamiento = (DateTime)Lector["FechaLanzamiento"];
+                    int CantidadCanciones = (int)Lector["CantidadCanciones"];
+                    string UrlImagen = (string)Lector["UrlImagenTapa"];
+                    string Estilo = (string)Lector["Descripcion"];
+                    string TipoEdicion = (string)Lector["EDICION"];
+
+                    Disco aux = new Disco(Id, Titulo, FechaLanzamiento, CantidadCanciones, UrlImagen, Estilo, TipoEdicion);
+                    Lista.Add(aux);
+                }
+                return Lista;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Conexion.Close();
+            }
+        }
+    }
+}
