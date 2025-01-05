df027e140fcfb435bb182be7c9ac2f6ff1030739
Lenadoss <odanelfrias@gmail.com>
2025-01-04 21:29:58 -0400

    Penultimo commit, solo falta arreglar el filtro rápido, se duplican los artículos en la DATA GRID VIEW.

---
+++ TPFinalNivel2_Alcántara/TPFinalNivel2_Alcántara/Negocio/CategoriaNegocio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        AccesoDatos Datos = new AccesoDatos();
+        private List<Categoria> Lista = new List<Categoria>();
+
+        public List<Categoria> Listar()
+        {
+            try
+            {
+                Datos.CrearConexion();
+                Datos.Consultar("SELECT Id, Descripcion FROM CATEGORIAS");
+                Datos.AbrirConexion();
+                using (SqlDataReader Lector = Datos.LeerConsulta())
+                    while (Lector.Read())
+                    {
+                        Categoria aux = new Categoria();
+                        aux.Id = (int)Lector["Id"];
+                        aux.Descripcion = (string)Lector["Descripcion"];
+                        Lista.Add(aux);
+                    }
+                return Lista;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Datos.CerrarConexion();
+            }
+        }
+    }
+}
---
+++ TPFinalNivel2_Alcántara/TPFinalNivel2_Alcántara/presentacion/FormAgregar.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace presentacion
+{
+    partial class FormAgregar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.cbBoxMarca = new System.Windows.Forms.ComboBox();
+            this.cbBoxCategoria = new System.Windows.Forms.ComboBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtImagen = new System.Windows.Forms.TextBox();
+            this.lblImagen = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.gBoxAgregar = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.p = new System.Windows.Forms.PictureBox();
+            this.gBoxAgregar.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(16, 38);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(49, 15);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(91, 35);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(147, 21);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCodigo_KeyPress);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 61);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(147, 21);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(16, 64);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(55, 15);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(91, 87);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(147, 21);
+            this.txtDescripcion.TabIndex = 5;
+            this.txtDescripcion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDescripcion_KeyPress);
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(16, 90);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(75, 15);
+            this.lblDescripcion.TabIndex = 4;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(16, 116);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(45, 15);
+            this.lblMarca.TabIndex = 6;
+            this.lblMarca.Text = "Marca:";
+            // 
+            // cbBoxMarca
+            // 
+            this.cbBoxMarca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBoxMarca.FormattingEnabled = true;
+            this.cbBoxMarca.Location = new System.Drawing.Point(91, 113);
+            this.cbBoxMarca.Name = "cbBoxMarca";
+            this.cbBoxMarca.Size = new System.Drawing.Size(147, 23);
+            this.cbBoxMarca.TabIndex = 8;
+            // 
+            // cbBoxCategoria
+            // 
+            this.cbBoxCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBoxCategoria.FormattingEnabled = true;
+            this.cbBoxCategoria.Location = new System.Drawing.Point(91, 140);
+            this.cbBoxCategoria.Name = "cbBoxCategoria";
+            this.cbBoxCategoria.Size = new System.Drawing.Size(147, 23);
+            this.cbBoxCategoria.TabIndex = 10;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(16, 143);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(63, 15);
+            this.lblCategoria.TabIndex = 9;
+            this.lblCategoria.Text = "Categoría:";
+            // 
+            // txtImagen
+            // 
+            this.txtImagen.Location = new System.Drawing.Point(91, 167);
+            this.txtImagen.Name = "txtImagen";
+            this.txtImagen.Size = new System.Drawing.Size(147, 21);
+            this.txtImagen.TabIndex = 12;
+            this.txtImagen.Leave += new System.EventHandler(this.txtImagen_Leave);
+            // 
+            // lblImagen
+            // 
+            this.lblImagen.AutoSize = true;
+            this.lblImagen.Location = new System.Drawing.Point(16, 170);
+            this.lblImagen.Name = "lblImagen";
+            this.lblImagen.Size = new System.Drawing.Size(71, 15);
+            this.lblImagen.TabIndex = 11;
+            this.lblImagen.Text = "Url Imagen:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(91, 193);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(147, 21);
+            this.txtPrecio.TabIndex = 14;
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(16, 196);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(45, 15);
+            this.lblPrecio.TabIndex = 13;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // gBoxAgregar
+            // 
+            this.gBoxAgregar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gBoxAgregar.Controls.Add(this.groupBox1);
+            this.gBoxAgregar.Controls.Add(this.lblCodigo);
+            this.gBoxAgregar.Controls.Add(this.txtPrecio);
+            this.gBoxAgregar.Controls.Add(this.txtCodigo);
+            this.gBoxAgregar.Controls.Add(this.lblPrecio);
+            this.gBoxAgregar.Controls.Add(this.lblNombre);
+            this.gBoxAgregar.Controls.Add(this.txtImagen);
+            this.gBoxAgregar.Controls.Add(this.txtNombre);
+            this.gBoxAgregar.Controls.Add(this.lblImagen);
+            this.gBoxAgregar.Controls.Add(this.lblDescripcion);
+            this.gBoxAgregar.Controls.Add(this.cbBoxCategoria);
+            this.gBoxAgregar.Controls.Add(this.txtDescripcion);
+            this.gBoxAgregar.Controls.Add(this.lblCategoria);
+            this.gBoxAgregar.Controls.Add(this.lblMarca);
+            this.gBoxAgregar.Controls.Add(this.cbBoxMarca);
+            this.gBoxAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBoxAgregar.Location = new System.Drawing.Point(53, 18);
+            this.gBoxAgregar.Name = "gBoxAgregar";
+            this.gBoxAgregar.Size = new System.Drawing.Size(252, 307);
+            this.gBoxAgregar.TabIndex = 15;
+            this.gBoxAgregar.TabStop = false;
+            this.gBoxAgregar.Text = "Agregar";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Location = new System.Drawing.Point(39, 239);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(175, 53);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(87, 19);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 16;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(6, 19);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 15;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // p
+            // 
+            this.p.Location = new System.Drawing.Point(311, 49);
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(259, 245);
+            this.p.TabIndex = 16;
+            this.p.TabStop = false;
+            // 
+            // FormAgregar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 343);
+            this.Controls.Add(this.p);
+            this.Controls.Add(this.gBoxAgregar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAgregar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agregar";
+            this.Load += new System.EventHandler(this.FormAgregar_Load);
+            this.gBoxAgregar.ResumeLayout(false);
+            this.gBoxAgregar.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.ComboBox cbBoxMarca;
+        private System.Windows.Forms.ComboBox cbBoxCategoria;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.TextBox txtImagen;
+        private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.GroupBox gBoxAgregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox p;
+    }
+}
